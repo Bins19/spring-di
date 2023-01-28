8c71f73eb286382c51b689c08bb410c90fb23b0f
Bins19 <badianebina@gmail.com>
2023-01-28 14:15:43 +0100

    Spring Profiles

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgdi;
 
-import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
-import guru.springframework.sfgdi.controllers.MyController;
-import guru.springframework.sfgdi.controllers.PropertyInjectedController;
-import guru.springframework.sfgdi.controllers.SetterInjectedController;
+import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +10,9 @@ public class SfgDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
+
+		I18NController i18NController = (I18NController) context.getBean("i18NController");
+		System.out.println(i18NController.getGreeting());
 
 		MyController myController = (MyController) context.getBean("myController");
 		System.out.println("-----Primary Bean-----");
---
+++ src/main/java/guru/springframework/sfgdi/services/I18NSpanishService.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("ES")
+@Service("i18NService")
+public class I18NSpanishService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hola Mundo - ES";
+    }
+
+}
